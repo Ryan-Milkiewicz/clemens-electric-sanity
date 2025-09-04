fe2bf22022b45caa4bbffafdba594d4d0a88320c
rmilkiewicz <ryan.milkiewicz@esd.ny.gov>
2025-09-04 15:23:27 -0400

    made some updates to service schema

--- schemaTypes/Service.ts
+++ schemaTypes/Service.ts
@@ -25,10 +25,28 @@ export const Service = defineType({
       of: [{type: 'block'}],
       validation: (rule) => rule.required(),
     }),
-    // defineField({
-    //   title: 'Icon',
-    //   name: 'icon',
-    //   type: 'image',
-    // }),
+    defineField({
+      title: 'Image',
+      name: 'image',
+      type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+          description: 'Important for SEO and accessibility.',
+          validation: (rule) => {
+            // Custom validation to ensure alt text is provided if the image is present. https://www.sanity.io/docs/validation
+            return rule.custom((alt, context) => {
+              if ((context.document?.coverImage as any)?.asset?._ref && !alt) {
+                return 'Required'
+              }
+              return true
+            })
+          },
+        },
+      ],
+      validation: (rule) => rule.required(),
+    }),
   ],
 })
